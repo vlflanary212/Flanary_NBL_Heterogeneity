48d66e6a177adc855f189db16c3daff74edf145c
Victoria L. Flanary <85571685+vlflanary212@users.noreply.github.com>
2025-02-08 08:48:14 -0600

    add stacked bar plots

--- NBAtlas/05_cluster_analyses/03_stacked_bar/ne_stacked_bar.R
+++ NBAtlas/05_cluster_analyses/03_stacked_bar/ne_stacked_bar.R
@@ -1,5 +1,5 @@
 # Author: Victoria Flanary
-# Date: 250205
+# Date: 250207
 # Objective: Visualize distribution of tumor cell clusters across patient
 # risk group and treatment timepoint.
 
@@ -14,7 +14,7 @@ data_dir <- here("NBAtlas", "data", "alldata")
 results_dir <- here("NBAtlas", "05_cluster_analyses", "03_stacked_bar")
 
 ## Load data
-seurat_obj <- readRDS(file.path(data_dir, "09_ne_subset.rds"))
+seurat_obj <- readRDS(file.path(data_dir, "08_final_anno.rds"))
 
 # Set current ident to Cell_type
 seurat_obj$Cell_type <- seurat_obj@active.ident
@@ -24,7 +24,7 @@ metadata <- seurat_obj@meta.data
 
 # Format metadata to add Westermann subgroups (combines Risk and MYCN status)
 ### Edit Risk column 
-unique(metadata$Risk)  # "Low/Intermediate" "High"             "_unknown"   
+unique(metadata$Risk)  # "Low/Intermediate" "High"             "unknown"   
 metadata$Risk <- ifelse(metadata$Risk == "Low/Intermediate", "Low", metadata$Risk)
 metadata$Risk <- ifelse(metadata$Risk == "unknown", "Unknown", metadata$Risk)
 metadata$Risk <- factor(metadata$Risk, levels = c("High", "Low", "Unknown"))
@@ -52,8 +52,8 @@ p <- ggplot(
   scale_y_continuous(labels = scales::percent) +
   labs(
     title = "MYCN Amplification by Risk",
-    x = "Risk",
-    y = "Percentage",
+    x = "Risk Level",
+    y = "Percentage of Tumors",
     fill = "MYCN Amplification"
   ) +
   theme_minimal() +
@@ -73,8 +73,6 @@ metadata <- metadata |>
     paste0(Risk, "-Risk", "_", "MYCN-", MYCN_amplification), "Unknown")) |>
   relocate(Group, .after = Risk)
 
-metadata$Group <- ifelse(metadata$Group == "Unknown-Risk_MYCN-unknown", "Unknown", metadata$Group)
-
 ### Plot the number of tumors in each Westermann Group on bar plot
 #### Select the relevant columns and summarize data
 metadata_summary <- metadata[, c("Sample", "Group")] |>  # Select Sample and Group column
@@ -92,7 +90,7 @@ adjusted_max_y <- max_y * 1.1
 #### Create and print the plot
 pdf(
   file.path(results_dir, "num_tumors_per_westermann_group.pdf"),
-  height = 4, width = 6
+  height = 4, width = 6.25
 )
 
 print(
@@ -101,9 +99,9 @@ print(
     geom_text(aes(label = n), vjust = -0.5, size = 4) +
     ylim(0, adjusted_max_y) +
     labs(
-      title = "Number of Samples by Westermann Group",
+      title = "Number of Tumors by Westermann Group",
       x = NULL,
-      y = "Number of Samples"
+      y = "Number of Tumors"
     ) +
     theme_minimal() +
     theme(
@@ -115,11 +113,11 @@ print(
 
 dev.off()
 
-### Save the tumor seurat object with updated metadata
-seurat_obj@meta.data <- metadata
-saveRDS(seurat_obj, file.path(data_dir, "10_ne_westermann.rds"))
-
 # Determine the distribution of tumor cell clusters across Westermann groups
+## Subset to only include tumor/neuroendocrine clusters
+ne_clusters <- paste0("NE_", 1:15)
+metadata <- subset(metadata, Cell_type %in% ne_clusters)
+
 ## Calculate proportion data
 proportion_data <- metadata |>
   group_by(Group, Cell_type) |>
@@ -137,19 +135,23 @@ p <- ggplot(
   proportion_data,
   aes(x = Group, y = percentage, fill = Cell_type)
 ) +
-  geom_bar(stat = "identity") +
+  geom_bar(stat = "identity", color = "darkgoldenrod") +
+  geom_text(
+    aes(label = ifelse(percentage >= 0.01, as.character(Cell_type), "")),
+    position = position_stack(vjust = 0.5),
+    size = 2.5 
+  ) +
   scale_y_continuous(labels = scales::percent) +
   labs(
     title = "Tumor Cell Clusters by Westermann Group",
-    x = "Westermann Group",
-    y = "Percentage",
-    fill = "Tumor Cluster"
+    x = NULL,
+    y = "Percentage of Tumor Cells"
   ) +
   theme_minimal() +
   theme(
     plot.title = element_text(hjust = 0.5),
     axis.text.x = element_text(angle = 45, hjust = 1) 
-  )
+  ) + NoLegend()
 
 print(p)
 
@@ -157,29 +159,15 @@ dev.off()
 
 
 # Determine tumor cell clusters across treatment timepoints
-## Filter for patients from which multiple tumor samples were collected
-metadata_summary <- metadata[, c("Sample", "Patient_No")] |> 
-  distinct() |>
-  group_by(Patient_No) |>   
-  summarise(n = n(), .groups = "drop") |>
-  filter(n >= 2)
-
-
-metadata_filt <-  metadata |> 
-  filter(Patient_No %in% metadata_summary$Patient_No) |>
-  dplyr::select(Patient_No, Group, Timepoint, Cell_type) |> 
-  distinct() |> 
-  arrange(Patient_No, Timepoint) 
-
-metadata_filt$Timepoint <- factor(metadata_filt$Timepoint, levels = c("pre-treatment", "post-treatment", "relapse"))
-
 ## Calculate proportion data
-proportion_data <- metadata_filt |>
+proportion_data <- metadata |>
   group_by(Timepoint, Cell_type) |>
   summarize(count = n(), .groups = "drop") |>
   group_by(Timepoint) |>
   mutate(percentage = count / sum(count)) |>
   ungroup()
+
+proportion_data$Timepoint <- factor(proportion_data$Timepoint, levels = c("pre-treatment", "post-treatment", "relapse", "unknown"))
 
 ## Plot the results on a stacked bar plot
 pdf(
@@ -190,26 +178,45 @@ p <- ggplot(
   proportion_data,
   aes(x = Timepoint, y = percentage, fill = Cell_type)
 ) +
-  geom_bar(stat = "identity") +
+  geom_bar(stat = "identity", color = "darkgoldenrod") +
+  geom_text(
+    aes(label = ifelse(percentage >= 0.01, as.character(Cell_type), "")),
+    position = position_stack(vjust = 0.5),
+    size = 2.5
+  ) +
   scale_y_continuous(labels = scales::percent) +
   labs(
     title = "Tumor Cell Clusters by Treatment Timepoint",
-    x = "Timepoint",
-    y = "Percentage",
-    fill = "Tumor Cluster"
+    x = NULL,
+    y = "Percentage of Tumor Cells"
   ) +
   theme_minimal() +
   theme(
     plot.title = element_text(hjust = 0.5),
     axis.text.x = element_text(angle = 45, hjust = 1) 
-  )
+  ) + NoLegend()
 
 print(p)
 
 dev.off()
 
-## Generate the same plot for each individual patient in metadata_filt
-# Open a single PDF file to save all plots
+## Generate the same plot for each patient with paired pre- and post-treatment data (30 and 32)
+# Format metadata for patients with paired data
+metadata_summary <- metadata[, c("Sample", "Patient_No")] |>
+  distinct() |>
+  group_by(Patient_No) |>
+  summarise(n = n(), .groups = "drop") |>
+  filter(Patient_No %in% c(30, 32))
+
+metadata_filt <-  metadata |>
+  filter(Patient_No %in% metadata_summary$Patient_No) |>
+  dplyr::select(Patient_No, Group, Timepoint, Cell_type) |>
+  distinct() |>	
+  arrange(Patient_No, Timepoint)
+
+metadata_filt$Timepoint <- factor(metadata_filt$Timepoint, levels = c("pre-treatment", "post-treatment"))
+
+# Plot the tumor cluster distribution per patient
 pdf(
   file.path(results_dir, "ne_clusters_by_treatment_timepoint_by_patient.pdf")
 )
@@ -231,25 +238,27 @@ for (patient in unique(metadata_filt$Patient_No)) {
     patient_data,
     aes(x = Timepoint, y = percentage, fill = Cell_type)
   ) +
-    geom_bar(stat = "identity") +
+    geom_bar(stat = "identity", color = "darkgoldenrod") +
+    geom_text(
+    aes(label = Cell_type),
+    position = position_stack(vjust = 0.5),
+    size = 3  # Adjust size for readability
+    ) +
     scale_y_continuous(labels = scales::percent) +
     labs(
       title = paste("Tumor Cell Clusters by Treatment Timepoint - Patient", patient),
-      x = "Timepoint",
-      y = "Percentage",
-      fill = "Tumor Cluster"
+      x = NULL,
+      y = "Percentage of Tumor Cells"
     ) +
     theme_minimal() +
     theme(
       plot.title = element_text(hjust = 0.5),
       axis.text.x = element_text(angle = 45, hjust = 1)
-    )
+    ) + NoLegend()
   
-  # Print the plot to the PDF device
   print(p)
 }
 
-# Close the PDF file after all plots are added
 dev.off()
 
 # End of script
